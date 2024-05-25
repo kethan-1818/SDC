9b8a62827b277c6791a80d3e134befdecf09ea9c
Aditi Arana <139562121+Aditi-Arana@users.noreply.github.com>
2024-05-25 14:28:17 +0530

    Update icons.jsx

--- client/src/asset/icons/icons.jsx
+++ client/src/asset/icons/icons.jsx
@@ -4,13 +4,13 @@ export function LinkedIn(props) {
 		<svg width="1em" height="1em" viewBox="0 0 24 24" className={props}>
 			<path
 				fill="currentColor"
-				d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"
+				d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"
 			/>
+			<rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/>
 		</svg>
 		</a>
 	);
 }
-
 export function Instagram(props) {
 	return (
 		<a href="https://www.instagram.com/sdc_nitw/" target="_blank" rel="noopener noreferrer">

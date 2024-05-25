378eefe1ee8a8ab803fde543ec0c8d3601f15df2
Ankulgupta5284 <137998358+Ankulgupta5284@users.noreply.github.com>
2024-05-25 13:07:32 +0530

    Update Homepage.jsx

--- client/src/pages/home/Homepage.jsx
+++ client/src/pages/home/Homepage.jsx
@@ -1,11 +1,15 @@
 import About from "./components/about/About";
 import ContactUs from "./components/contact-us/ContactUs";
+import "./homepage.css";
 
 function Home() {
 	return (
-		<>
-			<About />
-			<ContactUs />
+		<> 
+		<div className="mid">
+		<About className="about"/>
+		<ContactUs />
+		</div>
+			
 		</>
 	);
 }

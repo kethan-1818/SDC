2a940dc4d801a20cf487c645142a3758f5f5af26
Ankulgupta5284 <137998358+Ankulgupta5284@users.noreply.github.com>
2024-05-25 16:02:06 +0530

    Add files via upload

---
+++ client/src/pages/domains/Coding.jsx
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+import "./domain.css";
+
+
+function Coding() {
+	return (
+		<p>THIS IS CODING DOMAIN </p>
+	);
+}
+
+export default Coding;
---
+++ client/src/pages/domains/Cybersecurity.jsx
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+import { useState } from "react";
+import "./domain.css";
+
+
+function Cybersecurity() {
+	return (
+		<p>THIS IS CyberSecurity DOMAIN </p>
+	);
+}
+
+export default Cybersecurity;

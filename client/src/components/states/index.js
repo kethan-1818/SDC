fb8b07250dc38fbd78598f3d437bb2e056198662
Ankulgupta5284 <137998358+Ankulgupta5284@users.noreply.github.com>
2024-05-30 21:21:05 +0530

    Delete client/src directory

--- client/src/components/states/index.js
+++
@@ -1,2 +0,0 @@
-export { default as LoadingState } from "./loading";
-export { default as ErrorState } from "./error";
--- client/src/services/auth/index.jsx
+++
@@ -1,9 +0,0 @@
-import axiosInstance from "../axios/index";
-
-function LoginService(email) {
-	const response = axiosInstance.post(`/auth/login`, {
-		destination: JSON.stringify(email),
-	});
-}
-
-export default LoginService;

7bad8e925aa262543ad4bf3650c4776436639bc7
Aditi Arana <139562121+Aditi-Arana@users.noreply.github.com>
2024-06-14 22:05:28 +0530

    Update About.jsx

--- client/src/pages/home/components/about/About.jsx
+++ client/src/pages/home/components/about/About.jsx
@@ -5,12 +5,12 @@ import 'react-awesome-slider/dist/styles.css';
 import './about.css';
 
 // Import local images
-import image1 from '../../../events/images/DBMS.jpg';
-import image2 from '../../../events/images/DBMS.jpg';
-import image3 from '../../../events/images/DBMS.jpg';
-import image4 from '../../../events/images/DBMS.jpg';
-import image5 from '../../../events/images/DBMS.jpg';
-import image6 from '../../../events/images/DBMS.jpg';
+import image1 from '../../../events/images/tech_memers2.png';
+import image2 from '../../../events/images/codemaze.png';
+import image3 from '../../../events/images/aiverse2.png';
+import image4 from '../../../events/images/apectf.png';
+import image5 from '../../../events/images/dbms.png';
+import image6 from '../../../events/images/fac_ad_sdc.png';
 
 function About() {
   const images = [image1, image2, image3, image4, image5, image6];
